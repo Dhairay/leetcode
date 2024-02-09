a4f27fceb3f173d6a40817a008b6083328e5b615
Dhairay <dhairaykansal@gmail.com>
2024-02-09 17:19:49 +0530

    New Code added

--- src/practice/leetcode/Sliding_Window/LongestSubstringWithoutRepeatingCharacters.java
+++ src/practice/leetcode/Sliding_Window/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,2 +1,31 @@
-package practice.leetcode.Sliding_Window;public class LongestSubstringWithoutRepeatingCharacters {
+package practice.leetcode.Sliding_Window;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+        System.out.println(lengthOfLongestSubstring(s));
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        int i = 0;
+        int j = 0;
+        int maxL = 0;
+        List<Character> li = new ArrayList<>();
+        while (j < s.length()) {
+            if (!li.contains(s.charAt(j))) {
+                li.add(s.charAt(j));
+                j++;
+                maxL = Math.max(maxL, li.size());
+            } else {
+                li.remove(Character.valueOf(s.charAt(i)));
+                i++;
+            }
+        }
+
+
+        return maxL;
+    }
 }
